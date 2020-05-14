0118ce8a355d7f580b4a16667b9dc9fedf231a2c
Jorge <sandix390@gmail.com>
2020-05-14 14:42:53 -0500

    Added RayCast To Gun

---
+++ The Wave/Assets/Scripts/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    //Cached references
+    [SerializeField] private Camera mainCamera;
+
+    //Config Values
+    [SerializeField] float shootingDistance = 80f;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        PlayerInput();
+    }
+
+
+    private void PlayerInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Physics.Raycast(this.mainCamera.transform.position, this.mainCamera.transform.forward, out RaycastHit hit, this.shootingDistance);
+        Debug.Log("Colision con: " +  hit.transform.name);
+    }
+}
