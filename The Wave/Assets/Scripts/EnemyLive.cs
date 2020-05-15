5cb2c3f866e545f75255cd514d6c7374d8d5b40b
Jorge <sandix390@gmail.com>
2020-05-14 20:33:26 -0500

    Added Weapon Scope

--- The Wave/Assets/Scripts/EnemyLive.cs
+++ The Wave/Assets/Scripts/EnemyLive.cs
@@ -13,6 +13,7 @@ public class EnemyLive : MonoBehaviour
     public void HitTaken(float damage)
     {
         this.gameObject.GetComponent<EnemyAI>().onShootTaken();
+        //BroadcastMessage("onShootTaken");
         this.maxHealt -= damage;
         if(this.maxHealt <= 0)
         {
---
+++ The Wave/Assets/Scripts/WeaponScope.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScope : MonoBehaviour
+{
+    //Cached References
+    [SerializeField] Camera mainCamera;
+
+    //Config Values
+    [SerializeField] float scopeOut = 60;
+    [SerializeField] float scopeIn = 30;
+    private bool isScope = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.isScope = !this.isScope;
+            if (isScope)
+            {
+                this.mainCamera.fieldOfView = this.scopeIn;
+            }
+            else
+            {
+                this.mainCamera.fieldOfView = this.scopeOut;
+            }
+        }
+    }
+
+
+
+
+
+}
