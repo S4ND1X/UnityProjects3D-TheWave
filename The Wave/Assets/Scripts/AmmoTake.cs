11682c435c428f04003b09f3561d3a6c193316af
Jorge <sandix390@gmail.com>
2020-05-15 12:25:47 -0500

    Bug Fixed with Different types of ammo

---
+++ The Wave/Assets/Scripts/AmmoTake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoTake : MonoBehaviour
+{
+    [SerializeField] private int ammoAmount = 10;
+    [SerializeField] private AmmoWeapon ammoWeapon;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Ammo tmp = FindObjectOfType<Ammo>();//Finds the component Ammo on the gameObject that collide with
+            tmp.SetAmmount(tmp.GetAmmount(this.ammoWeapon) + this.ammoAmount, this.ammoWeapon);//Increase the ammount of ammo of that type 
+            Destroy(gameObject);
+        }
+    }
+
+}
